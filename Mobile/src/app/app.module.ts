4f5900ed949c4b884588195017ee6b937a90c0f7
Mucilontake <rmoraes33@hotmail.com>
2017-10-26 11:30:58 -0200

    Logo, app, rotas, barcode

--- Mobile/src/app/app.module.ts
+++ Mobile/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+
 
 import { MyApp } from './app.component';
 import { MenuPage } from '../pages/menu/menu';
@@ -24,6 +26,7 @@ import { HttpModule } from '@angular/http';
     LoginPage,
     CarrinhoPage,
     HistoricoPage
+    
   ],
   imports: [
     BrowserModule,
@@ -44,6 +47,7 @@ import { HttpModule } from '@angular/http';
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     RestProvider,
+    BarcodeScanner
   ]
 })
 export class AppModule {}
--- Mobile/src/pages/carrinho/carrinho.ts
+++ Mobile/src/pages/carrinho/carrinho.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+
 
 @IonicPage()
 @Component({
@@ -8,12 +10,20 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CarrinhoPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+              private barcodeScanner: BarcodeScanner) {
   }
 
   ionViewDidLoad(){
     console.log('ionViewDidLoad CarrinhoPage');
   }
-
+  
+  getcode(){
+    this.barcodeScanner.scan().then((barcodeData) => {
+    // Success! Barcode data is here
+   }, (err) => {
+       // An error occurred
+   });
+ }
 
 }
